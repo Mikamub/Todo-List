2ab0bbfcbd3598ec679ecbd223490f997c3e03c6
Mika <mikamub@gmail.com>
2024-01-05 11:59:45 -0300

    CSS y pop up

---
+++ src/TodoForm/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import 'TodoForm.css';
+
+function TodoForm(){
+    return(
+        <form>
+            <label>Escribe tu nuevo ToDo</label>
+            <textarea
+                placeholder="Cortar cebolla"
+            />
+            <div className="TodoForm-buttonContainer">
+                <button className="TodoForm-button 
+                TodoForm-button--cancel"
+                >Cancelar</button>
+                <button className="TodoForm-button
+                TodoForm-button--add"
+                >Añadir</button>
+            </div>
+
+        </form>
+    )
+}
+
+export {TodoForm};
